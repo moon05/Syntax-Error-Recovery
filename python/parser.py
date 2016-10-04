1ccc8d25f65905740cb8c287cf0a0d6235160c2c
Abdullah Al Mamun <Moon@Abdullahs-MacBook-Pro.local>
2016-10-03 21:16:21 -0400

    almost there with the python version, match and scan not fully working yet

--- python/parser.py
+++ python/parser.py
@@ -2,7 +2,10 @@ import math
 import sys
 
 global input_token;
-global image = '';
+global image;
+global syntax_tree;
+
+image = '';
 
 names = ["read", "write", "if", "do", "fi", "od",
 			"od", "check", "id", "literal","gets",
@@ -17,27 +20,116 @@ tokens = {"t_read":0, "t_write":1, "t_if":2, "t_do":3, "t_fi":4,
 			"t_less_eq":14, "t_great_eq":15, "t_add":16, "t_sub":17,
 			"t_mul":18, "t_div":19, "t_lparen":20, "t_rparen":21,"t_eof":22}
 
+#first sets
+P = ["t_id", "t_read", "t_write", "t_if", "t_do", "t_check", "t_eof"]
+SL = ["t_id", "t_read", "t_write", "t_if", "t_do", "t_check"]
+S = ["t_id", "t_read", "t_write", "t_if", "t_do", "t_check"]
+R = ["t_lparen", "t_id", "t_literal"]
+E = ["t_lparen", "t_id", "t_literal"]
+T = ["t_lparen", "t_id", "t_literal"]
+F = ["t_lparen", "t_id", "t_literal"]
+ET = ["t_eq_eq", "t_not_eq", "t_less", "t_great", "t_less_eq", "t_great_eq"]
+TT = ["t_add", "t_sub"]
+FT = ["t_mul", "t_div"]
+ro = ["t_eq_eq", "t_not_eq", "t_less", "t_great", "t_less_eq", "t_great_eq"]
+ao = ["t_add", "t_sub"]
+mo = ["t_mul", "t_div"]
+
+#epsilons
+epsP = False;
+epsSL = True;
+epsS = False;
+epsR = False;
+epsE = False;
+epsT = False;
+epsF = False;
+epsET = True;
+epsTT = True;
+epsFT = True;
+eps_ro = False;
+eps_ao = False;
+eps_mo = False;
+
+#follow sets
+P_follow = []
+SL_follow = ["t_fi", "t_od", "t_eof"]
+S_follow = ["t_id", "t_read", "t_write", "t_if", "t_do", "t_check", "t_fi", "t_od", "t_eof"]
+R_follow = ["t_rparen", "t_id", "t_read", "t_write", "t_if", "t_do", "t_check", "t_fi", "t_od", "t_eof"]
+E_follow = ["t_eq_eq", "t_not_eq", "t_less", "t_great", "t_less_eq", "t_great_eq", "t_rparen","t_id", "t_read", "t_write", "t_if", "t_do", "t_check", "t_fi", "t_od", "t_eof"]
+T_follow = ["t_add", "t_sub", "t_eq_eq", "t_not_eq", "t_less", "t_great", "t_less_eq", "t_great_eq", "t_rparen","t_id", "t_read", "t_write", "t_if", "t_do", "t_check", "t_fi", "t_od", "t_eof"]
+F_follow = ["t_mul", "t_div", "t_add", "t_sub", "t_eq_eq", "t_not_eq", "t_less", "t_great", "t_less_eq", "t_great_eq", "t_rparen","t_id", "t_read", "t_write", "t_if", "t_do", "t_check", "t_fi", "t_od", "t_eof"]
+ET_follow = ["t_rparen", "t_id", "t_read", "t_write", "t_if", "t_do", "t_check", "t_fi", "t_od", "t_eof"]
+TT_follow = ["t_literal", "t_eq_eq", "t_not_eq", "t_less", "t_great", "t_less_eq", "t_great_eq", "t_rparen", "t_id", "t_read", "t_write", "t_if", "t_do", "t_check", "t_fi", "t_od", "t_eof"]
+FT_follow = ["t_add", "t_sub", "t_eq_eq", "t_not_eq", "t_less", "t_great", "t_less_eq", "t_great_eq", "t_rparen",
+                    "t_id", "t_read", "t_write", "t_if", "t_do", "t_check", "t_fi", "t_od", "t_eof"]
+ro_follow = ["t_lparen", "t_id", "t_literal"]
+ao_follow = ["t_lparen", "t_id", "t_literal"]
+mo_follow = ["t_lparen", "t_id", "t_literal"]
+
+#error reporting
+def error():
+	print "syntax error"
+	sys.exit(0);
+
+def report_error(tok):
+	print "Invalid symbol: %s" %  names[tokens[tok]]
+
 #scan and other helper function
 def scanMatcher(str):
 	if (str is "read"):
 		return tokens["t_read"]
 	elif (str is "write"):
 		return tokens["t_write"]
-	elif (str is )
+	elif (str is "$$"):
+		return tokens["t_eof"]
+	elif (str is "if"):
+		return tokens["t_if"]
+	elif (str is "fi"):
+		return tokens["t_fi"]
+	elif (str is "do"):
+		return tokens["t_do"]
+	elif (str is "od"):
+		return tokens["t_od"]
+	elif (str is "check"):
+		return tokens["t_check"]
+	elif (str is ":="):
+		return tokens["t_gets"]
+	elif (str is "=="):
+		return tokens["t_eq_eq"]
+	elif (str is "<>"):
+		return tokens["t_not_eq"]
+	elif (str is "<"):
+		return tokens["t_less"]
+	elif (str is ">"):
+		return tokens["t_great"]
+	elif (str is "<="):
+		return tokens["t_less_eq"]
+	elif (str is ">="):
+		return tokens["t_great_eq"]
+	elif (str is "+"):
+		return tokens["t_add"]
+	elif (str is "-"):
+		return tokens["t_sub"]
+	elif (str is "/"):
+		return tokens["t_div"]
+	elif (str is "*"):
+		return tokens["t_mul"]
+	elif (str is "("):
+		return tokens["t_lparen"]
+	elif (str is ")"):
+		return tokens["t_rparen"]
+	else:
+		print "Invalid Character"
+		return sys.exit(0)
 
 def scan(str):
 	newStr  = str.split()
 	print newStr
-	return 
+	return
 
 
 
 
-#grammar rules
-
-def error():
-	print "syntax error"
-	sys.exit(0);
 
 def match(expected):
 	if (input_token is expected):
@@ -45,164 +137,352 @@ def match(expected):
 		if  (input_token is 7 or input_token is 8):
 			print ": %s" % image
 		print
+		syntax_tree += (names[expected] + " ")
 	else:
 		error()
 
 
+#error-recovery functions
+
+##takes in token(int) and token-list
+def isin(token, listy):
+	a = len(listy)
+	for i in listy:
+		if ( token is tokens[listy[i]] ):
+			return True;
+	return False;
+
+##takes in first-set, eps, follow-set
+def check_error(first, eps, follow):
+	if ( (not isin(input_token, first)) and eps):
+		return True
+	
+	if ( isin(input_token, first) ):
+		return False
+
+	report_error(input_token)
+	while ( (not isin(input_token, first) or isin(input_token, follow)) ):
+		input_token = scan()
+	if ( isin(input_token, first) ):
+		return False
+	else:
+		return True
+
+
+#grammar rules
+
 def program():
+	if (check_error (P, epsP, P_follow)):
+		program()
+	syntax_tree += "("
 	if (input_token is (tokens["t_id"] or tokens["t_read"] or tokens["t_write"] or tokens["t_do"] or tokens["t_if"] or tokens["t_check"] or tokens["t_eof"])):
 		print "predict program --> stmt_list eof"
 		stmt_list()
-		match( tokens["t_eof"] )
-		break
-	return
+		try:
+			match( tokens["t_eof"] )
+		except:
+			if ( check_error( ["eof"], False, P_follow) ):
+				return
+	else:
+		return
+
+	syntax_tree += ")"
 
 def stmt_list():
+	if ( check_error (SL, epsSL, SL_follow) ):
+		return
+	syntax_tree += "("
+
 	if (input_token is (tokens["t_id"] or tokens["t_read"] or tokens["t_if"] or tokens["t_do"] or tokens["t_check"] or tokens["t_write"])):
 		print "predict stmt_list --> stmt stmt_list"
 		stmt()
 		stmt_list()
-		break
-	return;
+	
+	else:
+		return
+
+	syntax_tree += ")"
 
 def stmt():
+	if ( check_error (S, epsS, S_follow) ):
+		return
+	syntax_tree += "("
+
 	if (input_token is tokens["t_id"]):
 		print "predict stmt --> id gets expr"
 		match( tokens["t_id"] )
 		match( tokens["t_gets"] )
 		expr()
-		break
 	elif (input_token is tokens["t_read"]):
 		print "predict stmt --> read id"
 		match( tokens["t_read"] )
-		match( token["t_id"] )
-		break
+		match( tokens["t_id"] )
+		
 	elif (input_token is tokens["t_write"]):
 		print "predict stmt --> write R_expr"
 		match( tokens["t_write"] )
 		R_expr()
-		break
+		
 	elif (input_token is tokens["t_if"]):
 		print "predict stmt --> if R_expr stmt_list fi"
 		match( tokens["t_if"] )
 		R_expr()
 		stmt_list()
 		match( tokens["t_fi"] )
-		break
+		
 	elif (input_token is tokens["t_do"]):
 		print "predict stmt --> do stmt_list od"
 		match( tokens["t_do"] )
 		stmt_list()
 		match( tokens["t_od"] )
-		break
+
 	elif (input_token is tokens["t_check"]):
 		print "predict stmt --> check R_expr"
 		match( tokens["t_check"] )
 		R_expr()
-		break
+		
 	return
 
 def R_expr():
+	if ( check_error (R, epsR, R_follow) ):
+		return
+	syntax_tree += "("
 	print "predict R_expr --> expr expr_tail"
 	expr()
 	expr_tail()
+	syntax_tree += ")"
 
 def expr():
+	if ( check_error (E, epsE, E_follow) ):
+		return
+	syntax_tree += "("
 	print "predict expr --> term term_tail"
 	term()
 	term_tail()
+	syntax_tree += ")"
 
 def term():
+	if ( check_error (T, epsT, T_follow) ):
+		return
+	syntax_tree += "("
 	print "predict term --> factor factor_tail"
 	factor()
 	factor_tail()
+	syntax_tree += ")"
 
 def factor():
+	if ( check_error (F, epsF, F_follow) ):
+		return
+	syntax_tree += "("
+
 	if (input_token is tokens["t_lparen"]):
 		print "predict factor --> lparen R_expr rparen"
-		match( tokens["t_lparen"] )
+		
+		try:
+			match( tokens["t_lparen"] )
+		except:
+			if ( check_error (["lparen"], False, F_follow) ):
+				return
+			else:
+				match( tokens["t_lparen"] )
+
 		R_expr()
-		match( tokens["t_rparen"] )
-		break
+		
+		try:
+			match( tokens["t_rparen"] )
+		except:
+			if ( check_error (["rparen"], False, F_follow) ):
+				return
+			else:
+				match( tokens["t_rparen"] )
+		
 	elif (input_token is tokens["t_id"]):
 		print "predict factor --> id"
-		match( token["t_id"] )
-		break
+		
+		try:
+			match( tokens["t_id"] )
+		except:
+			if ( check_error (["id"], False, F_follow) ):
+				return
+			else:
+				match( tokens["t_id"] )
+		
 	elif (input_token is tokens["t_literal"]):
 		print "predict factor --> literal"
-		match( token["t_literal"] )
-		break
-	return
+		
+		try:
+			match( tokens["t_literal"] )
+		except:
+			if ( check_error (["literal"], False, F_follow) ):
+				return
+			else:
+				match( tokens["t_literal"] )
+
+	else:
+		return
+
+	syntax_tree += ")"
+
 
 def expr_tail():
+	if ( check_error (ET, epsET, ET_follow) ):
+		return
+	syntax_tree += "("
+
 	if (input_token is (tokens["t_eq_eq"] or tokens["t_not_eq"] or tokens["t_less"] or tokens["t_great"] or tokens["t_less_eq"] or tokens["t_great_eq"])):
 		print "print expr_tail --> relational_op expr"
 		r_op()
 		expr()
-		break
-	return
+	else:
+		return
+	
+	syntax_tree += ")"
+
+def term_tail():
+	if ( check_error (TT, epsTT, TT_follow) ):
+		return
+	syntax_tree += "("
+
+	if ( input_token is ( tokens["t_add"] or tokens["t_sub"] ) ):
+		print "predict term_tail --> add_op term term_tail"
+		add_op()
+		term()
+		term_tail()
+
+	else:
+		return
+
+	syntax_tree += ")"
 
 def factor_tail():
+	if ( check_error (FT, epsFT, FT_follow) ):
+		return
+	syntax_tree += "("
+
 	if ( input_token is ( tokens["t_mul"] or tokens["t_div"]) ):
 		print "predict factor_tail --> mul_op factor factor_tail"
 		mul_op()
 		factor()
 		factor_tail()
-		break
-	return
+	
+	else:
+		return
+		
+	syntax_tree += ")"
 
 def r_op():
+	if ( check_error (ro, eps_ro, ro_follow) ):
+		return
+	syntax_tree += "("
+	
 	if ( input_token is tokens["t_eq_eq"] ):
 		print "predict r_op --> equalequal"
-		match( tokens["t_eq_eq"] )
-		break
+		try:
+			match( tokens["t_eq_eq"] )
+		except:
+			if ( check_error (["equalequal"], False, ro_follow) ):
+				return
+		
 	elif ( input_token is tokens["t_not_eq"] ):
 		print "predict r_op --> notequal"
-		match( tokens["t_not_eq"] )
-		break
+		try:
+			match( tokens["t_not_eq"] )
+		except:
+			if ( check_error (["notequal"], False, ro_follow) ):
+				return
+
 	elif ( input_token is tokens["t_less"] ):
 		print "predict r_op --> lessthan"
-		match( tokens["t_less"] )
-		break
+		try:
+			match( tokens["t_less"] )
+		except:
+			if ( check_error (["lessthan"], False, ro_follow) ):
+				return
+
 	elif ( input_token is tokens["t_great"] ):
 		print "predict r_op --> greaterthan"
-		match( tokens["t_great"] )
-		break
+		try:
+			match( tokens["t_great"] )
+		except:
+			if ( check_error (["greaterthan"], False, ro_follow) ):
+				return
+
 	elif ( input_token is tokens["t_less_eq"] ):
 		print "predict r_op --> lessORequal"
-		match( tokens["t_less_eq"] )
-		break
+		try:
+			match( tokens["t_less_eq"] )
+		except:
+			if ( check_error (["lessORequal"], False, ro_follow) ):
+				return
+	
 	elif ( input_token is tokens["t_great_eq"] ):
 		print "predict r_op --> greaterORequal"
-		match( tokens["t_great_eq"] )
-		break
-	return
+		try:
+			match( tokens["t_great_eq"] )
+		except:
+			if ( check_error (["greaterORequal"], False, ro_follow) ):
+				return
+	
+	else:
+		return
+
+	syntax_tree += ")"
 
 def add_op():
+	if ( check_error (mo, eps_mo, mo_follow) ):
+		return
+	syntax_tree += "("
+	
 	if ( input_token is tokens["t_add"] ):
 		print "predict add_op --> add"
-		match( tokens["t_add"] )
-		break
+		try:
+			match( tokens["t_add"] )
+		except:
+			if ( check_error (["add"], False, ao_follow) ):
+				return
+	
 	elif ( input_token is tokens["t_sub"] ):
 		print "predict add_op --> sub"
-		match( tokens["t_sub"] )
-		break
-	return
+		try:
+			match( tokens["t_sub"] )
+		except:
+			if ( check_error (["sub"], False, ao_follow) ):
+				return
+	
+	else:
+		return
+
+	syntax_tree += ")"
 
 def mul_op():
+	if ( check_error (mo, eps_mo, mo_follow) ):
+		return
+	syntax_tree += "("
+	
 	if ( input_token is tokens["t_mul"] ):
 		print "predict mul_op --> mul"
-		match( tokens["t_mul"] )
-		break
+		try:
+			match( tokens["t_mul"] )
+		except:
+			if ( check_error (["mul"], False, mo_follow) ):
+				return
+
 	elif ( input_token is tokens["t_div"] ):
 		print "predict mul_op --> div"
-		match( tokens["t_div"] )
-		break
-	return
+		try:
+			match( tokens["t_div"] )
+		except:
+			if ( check_error (["div"], False, mo_follow) ):
+				return
+	
+	else:
+		return
+
+	syntax_tree += ")"
 
 
 def main():
-	input_token = scan()
+	input_token = scan("a")
 	program()
 	return
 
